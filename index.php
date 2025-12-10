e187119a74f21b71c94e9fde70510ffd452d89af
Moreno Razzoli <morrolinux@gmail.com>
2025-12-10 21:21:14 +0100

    update analytics url

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <head>
 
-  <script async defer data-website-id="d793bf3d-8b31-4c09-845f-c042e9481f26" src="https://analytics.morrolinux.it/umami.js"></script>
+  <script defer src="https://analytics.morrolinux.it/script.js" data-website-id="3e22c6fa-de57-42a4-a5d7-1b36a91a45a2"></script>
 
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
